995224109cd48bddacfbb9477342d18710836f26
Robin <derrop@outlook.de>
2020-08-26 21:12:24 +0200

    Initial commit

---
+++ gommestats/src/main/java/com/github/derrop/proxy/plugins/gomme/messages/Language.java
@@ -0,0 +1,25 @@
+package com.github.derrop.proxy.plugins.gomme.messages;
+
+public enum Language {
+
+    GERMANY,
+    AUSTRIA,
+    US,
+    FRANCE,
+    RUSSIA,
+    TURKISH,
+    NETHERLANDS,
+    POLISH,
+    NORWAY,
+    SPANISH,
+    PORTUGAL,
+    BRAZIL,
+    CZECH,
+    SWEDISH,
+    HUNGARIAN,
+    JAPANESE,
+    CROATIAN,
+    SWISS,
+    LUXEMBOURG
+
+}
